33faab299cb8139b65eeac73863d3cdf6d7ba1a2
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2022-07-19 20:07:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from urllib import response
 import qr
 
-
-
-product=['rice','cake','bread','carrot']
-
-while True:
-    print("Welcome")
-    t=input("What product do you need?")
-    text=t.split()
+def newProduct():
+    new=input("What product do you need?")
+    text=new.split()
     m=0
     for i in product:
         for j in text:
@@ -17,13 +13,45 @@ while True:
                 break
         if m==1:
             break
+    return need
+def end():
+    q=int(input("Shall I take you to the billing section, 1)Yes 2)No"))
+    if q==1 or q=="yes":
+        print("Go Straight")
+        quit=0
+        return quit
+    elif q==2 or q=="no":
+        print("See you next time")
+        quit=1
+        return quit
+    else:
+        print("Enter correct response")
+        end()
+
+product=['rice','cake','bread','carrot']
+print("Welcome")
+
+asd=1
+while True:
+    if asd == 2:
+        need='origin'
+    else:
+        need=newProduct()
+   
     # A=rice B=cake C=bread D=carrot
 
 
     l= qr.scan()
-    same='z'+need
+    same='z'+need+'Ignore'
     if l == same:
         print('you are standing in the right location, product: '+need+' is in front of you.')
+        asd=int(input("1)Continue shopping, 2)End shopping"))
+        if asd == 2:
+            quit=end()
+        if quit ==1:
+            break
+    elif 'Ignore' in l:
+        print('Go Straight')
     else:
         q=l.split("#")
         right=q[0]
